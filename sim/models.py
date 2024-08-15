1d3e2622cd1725e0ebf38f85b19dbb1d44dceb87
Hao Wang <wh.tenghe@gmail.com>
2024-08-15 17:02:06 +1000

    init: coding template compeled

--- sim/models.py
+++ sim/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Name(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        db_table = 'names'
