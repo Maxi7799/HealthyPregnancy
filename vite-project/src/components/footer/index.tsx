dd44d55ceccbe1790034e05825dc50b84a0db817
Hao Wang <wh.tenghe@gmail.com>
2024-08-25 19:24:53 +1000

    feat: add frontend styling

---
+++ vite-project/src/components/footer/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./index.css";
+import icon from "../../assets/f-icon.png";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <img
+            src={icon}
+            alt="Healthy Pregnancy Logo"
+            className="footer-logo"
+          />
+          <h2>Healthy Pregnancy</h2>
+        </div>
+        <div className="footer-center">
+          <div className="footer-column">
+            <h3>Our Health</h3>
+            <p>Education for Risk Reducing</p>
+            <p>Education for food health</p>
+            <p>Data Insight Australia</p>
+            <p>See if Food Health</p>
+            <p>Exercise Instructions</p>
+          </div>
+          <div className="footer-column">
+            <h3>Programs and Projects</h3>
+            <p>Identify Healthy Food</p>
+            <p>Food nutrition Calculator</p>
+            <p>Identify Risky Area</p>
+            <p>Check Your Action</p>
+          </div>
+        </div>
+        <div className="footer-right">
+          <h3>Contact Us</h3>
+          <p>Privacy Statement</p>
+          <p>
+            <a href="#terms-of-use" className="clickable">
+              Terms of Use
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- vite-project/src/components/main/index.tsx
+++ vite-project/src/components/main/index.tsx
@@ -1,32 +1,10 @@
 import "./index.css";
-import { Link } from "react-router-dom";
-import { Button, Flex } from "antd";
+
 
 export function MainBox() {
   return (
     <>
-      <div className="main-box">
-        {/* <div className="main-pic"></div> */}
-        <Flex gap="small" wrap>
-          <Button>
-            <Link to="/educational">Educational</Link>
-          </Button>
-          <Button>
-            <Link to="/dataInsight">data insight</Link>
-          </Button>
-        </Flex>
-
-        <div className="main-text">
-          Welcome to our dedicated resource for immigrant mothers. <br />
-          We provide vital information, support, and guidance on maternal
-          health, <br />
-          ensuring you and your baby thrive. <br />
-          Explore our expert tips, healthcare resources, <br />
-          and community support to navigate your pregnancy <br />
-          journey with confidence and peace of mind.
-        </div>
-        <div className="footer"></div>
-      </div>
+      sss
     </>
   );
 }
