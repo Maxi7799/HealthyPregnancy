9b316ac0aa13f81b1d36158e286f81eef8c1fadb
Hao Wang <wh.tenghe@gmail.com>
2024-09-04 22:21:04 +1000

    fix

--- vite-project/src/env.tsx
+++ vite-project/src/env.tsx
@@ -1,5 +1,5 @@
 //backend server connection
-export const rootAddress = "https://healthypregnancy.online";
+export const rootAddress = "http://3.26.145.97:8000";
 
 //localhost
 // const localAddress = "http://localhost:8000"
