022059153733a46a08c8946963af8a967060883e
Hao Wang <wh.tenghe@gmail.com>
2024-09-06 20:54:33 +1000

    fix

--- vite-project/env.tsx
+++ vite-project/env.tsx
@@ -1,5 +1,5 @@
 //backend server connection
-export const rootAddress = "https://healthypregancybackend.systems:8000";
+export const rootAddress = "https://healthypregancybackend.systems:8443";
 
 //localhost
 // const localAddress = "http://localhost:8000"
