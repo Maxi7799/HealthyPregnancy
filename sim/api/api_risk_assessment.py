dd570a6ff36a3da7f43195120acb721de91a698e
Hao Wang <wh.tenghe@gmail.com>
2024-09-07 16:09:46 +1000

    feat: add risk assessment api

--- sim/api/api_risk_assessment.py
+++ sim/api/api_risk_assessment.py
@@ -3,5 +3,5 @@ from ninja import NinjaAPI
 risk = NinjaAPI(version="3.0.0")
 
 @risk.get("/riskassessment")
-def risk_assessment(request):
-    return {}
+def risk_assessment(request, risk_json):
+    return risk_json
