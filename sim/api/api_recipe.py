1888c620bad85df7786bbda225415fdac0ab54af
Hao Wang <wh.tenghe@gmail.com>
2024-09-08 13:30:48 +1000

    add: change data model interface output data frame

---
+++ sim/api/api_recipe.py
@@ -0,0 +1,4 @@
+from ninja import NinjaAPI
+
+
+recipe = NinjaAPI(version="5.0.0")
--- sim/urls.py
+++ sim/urls.py
@@ -4,8 +4,10 @@ from .api.connection_test import test
 from .api.api_data_insight import datainsight
 from .api.api_risk_assessment import risk
 from .api.api_food import food
+from .api.api_recipe import recipe
 
 urlpatterns = [
+    path('recipe/', recipe.urls),
     path('food/', food.urls),
     path('risk/', risk.urls),
     path('datainsight/', datainsight.urls),
