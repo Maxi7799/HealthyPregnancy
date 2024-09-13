e9acc7c743cf797803b8ebc1a5eae89642052a60
Hao Wang <wh.tenghe@gmail.com>
2024-09-13 16:23:05 +1000

    docs: add data structure for food ingredient nutritions

--- sim/api/api_food.py
+++ sim/api/api_food.py
@@ -1,12 +1,56 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
+from sim.data_model.data_interface.get_food_nutrition import get_food_nutrition_data
 
 
 food = NinjaAPI(version="4.0.0")
 
 
-@food.get("/nutritioninfo")
-def get_food_nutrition(request):
-    return "US5.1: Ingredient Nutrition Information"
+class FoodNutritionDataSchema(Schema):
+    ingrs: list[str]
+    ingredient_list: list[str]
+
+
+@food.post("/nutritioninfo")
+def get_food_nutrition(request, payload: FoodNutritionDataSchema):
+    output = {
+        'ingrs': payload.ingrs,
+        'calories': [],
+        'weights': [],
+        'total_calories': 0,
+        'total_fat_digits': 0,
+        'total_fat_percent': 0,
+        'saturated_fat_digits': 0,
+        'saturated_fat_percent': 0,
+        'trans_fat_digits': 0,
+        'trans_fat_percent': 0,
+        'cholesterol_digits': 0,
+        'cholesterol_percent': 0,
+        'sodium_digits': 0,
+        'sodium_percent': 0,
+        'total_carbohydrate_digits': 0,
+        "total_carbohydrate_percent": 0,
+        'dietary_fiber_digits': 0,
+        "dietary_fiber_percent": 0,
+        'total_sugars_digits': 0,
+        'total_sugars_percent': 0,
+        'protein_digits': 0,
+        'protein_percent': 0,
+        'vitamin_digits': 0,
+        'vitamin_percent': 0,
+        'calcium_digits': 0,
+        'calcium_percent': 0,
+        'iron_digits': 0,
+        'iron_percent': 0,
+        'potassium_digits': 0,
+        'potassium_percent': 0
+    }
+    food_data_list = []
+    for ingr in payload.ingredient_list:
+        food_data_list.append(get_food_nutrition_data(ingr))
+
+    print(food_data_list[0]["calories"])
+
+    return output
 
 
 @food.get("/notrecomfood")
@@ -17,4 +61,3 @@ def get_notrecom_food_list(request):
 @food.get("/nutritioncalcu")
 def get_nutrition_calcuated(request):
     return "US5.3: Nutrition Calculator for Balanced Intake"
-
--- sim/data_model/data_interface/get_food_nutrition.py
+++ sim/data_model/data_interface/get_food_nutrition.py
@@ -3,17 +3,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-# call Edamam api for food nutrition data
-API_URL = "https://jsonplaceholder.typicode.com/posts"
+Edamam_URL = "https://api.edamam.com"
 
 
+def get_food_nutrition_data(ingr):
 
-def get(self, request, *args, **kwargs):
-    response = requests.get(API_URL)
+    params = {
+        'app_id': "f5122945",
+        'app_key': "f4d0d4b74ed080435e5faf94071f2e6d",
+        'ingr': ingr
+    }
+
+    response = requests.get(Edamam_URL + "/api/nutrition-data", params)
 
     if response.status_code == 200:
         data = response.json()
-        return Response(data, status=status.HTTP_200_OK)
+        return data
     else:
         return Response(
             {"error": "Failed to retrieve data from external API"},
