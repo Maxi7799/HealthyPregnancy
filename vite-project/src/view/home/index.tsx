71f7562598b57ca72a002a9b77519b1ea567bd33
Hao Wang <wh.tenghe@gmail.com>
2024-09-26 15:24:29 +1000

    feat: add 6 languages model

--- vite-project/src/view/home/index.tsx
+++ vite-project/src/view/home/index.tsx
@@ -1,8 +1,5 @@
 import { Footer } from "../../components/footer/index.tsx";
 import { Header } from "../../components/header/header.tsx";
-import { HomeSec } from "../../components/home-sec/index.tsx";
-import { HomeThird } from "../../components/home-third/index.tsx";
-import { MainBox } from "../../components/main/index.tsx";
 
 import { HomeMain } from "../../components/home-main/index.tsx";
 import { HomeTitle } from "../../components/home-title/index.tsx";
