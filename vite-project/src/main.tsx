b8d465f7c3d10fc87f87943a47f8b0587e964399
Hao Wang <wh.tenghe@gmail.com>
2024-09-26 14:40:21 +1000

    feat: add multi language model for en, zh, id

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -5,14 +5,39 @@ import App from "./App.tsx";
 
 import { store } from "./store/index";
 import { Provider } from "react-redux";
-// import '../src/store/index'
+import global_en from "./translation/en/global.json";
+import global_zh from "./translation/zh/global.json";
+import global_id from "./translation/id/global.json";
+import i18next from "i18next";
 import "./index.css";
+import { Global } from "@emotion/react";
+import { I18nextProvider } from "react-i18next";
+
+
+i18next.init({
+  interpolation: {escapeValue: false},
+  lng: "en",
+  resources:{
+    en:{
+      global: global_en
+    },
+    zh:{
+      global: global_zh
+    },
+    id:{
+      global: global_id
+    }
+  }
+})
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <I18nextProvider i18n={i18next}>
+          <App />
+        </I18nextProvider>
       </BrowserRouter>
     </Provider>
   </StrictMode>
