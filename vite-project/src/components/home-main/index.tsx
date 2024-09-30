c72fcf196164943f20f22438e82d1c55838ed3b3
Hao Wang <wh.tenghe@gmail.com>
2024-09-30 13:50:04 +1000

    fix:  changed  home page title margin

--- vite-project/src/components/home-main/index.tsx
+++ vite-project/src/components/home-main/index.tsx
@@ -46,38 +46,4 @@ export const HomeMain: React.FC = () => {
     </Carousel>
   );
 
-  // const contentStyle: React.CSSProperties = {
-  //   margin: 0,
-  //   height: "160px",
-  //   color: "#fff",
-  //   lineHeight: "160px",
-  //   textAlign: "center",
-  //   background: "#364d79",
-  // };
-
-  // const
-
-  // return (
-  // <>
-  {
-    /* <div className="home-main">
-        <div className="main-text">
-          {t("home.hero")}
-        </div>
-        <div className="home-welcome">welcome</div>
-        <div className="home-welcome home-text">
-          Complete this short questionnaire to assess your pregnancy health
-          risks and receive personalized guidance for a safer, healthier
-          pregnancy.
-        </div>
-        <div className="home-button">
-          <Link to="/risk-assessment">Start Risk Assessment</Link>
-        </div>
-        <div className="home-main-image">
-          <img src={homeImg}></img>
-        </div>
-      </div> */
-  }
-  // </>
-  // );
 };
