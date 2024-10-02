a763646474f3a30204bfdc8d43b748c989101e93
Hao Wang <wh.tenghe@gmail.com>
2024-10-02 16:39:48 +1000

    feat: add exercise overview page and exercie details page

--- vite-project/src/view/exercise/index.tsx
+++ vite-project/src/view/exercise/index.tsx
@@ -1,11 +1,78 @@
+import { Link } from "react-router-dom";
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header/header";
+import "./index.css"
 
 export function Exercise(){
+
+    const cards = [
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 1 description",
+        link: "/videopage1",
+      },
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 2 description",
+        link: "/card-2",
+      },
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 3 description",
+        link: "/card-3",
+      },
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 4 description",
+        link: "/card-4",
+      },
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 5 description",
+        link: "/card-5",
+      },
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 6 description",
+        link: "/card-6",
+      },
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 7 description",
+        link: "/card-7",
+      },
+      {
+        image: "https://via.placeholder.com/150",
+        description: "Card 8 description",
+        link: "/card-8",
+      },
+    ];
+
+
     return (
       <>
         <Header />
-        <p>Exercise</p>
+
+        <div className="container">
+          <h1 className="title">Pregnancy Wellness</h1>
+          <div className="card-container">
+            {cards.map((card, index) => (
+              <Link to={card.link} className="card-link" key={index}>
+                <div className="card">
+                  <div className="image-container">
+                    <img
+                      src={card.image}
+                      alt={card.description}
+                      className="card-image"
+                    />
+                  </div>
+                  <div className="card-title">{card.description}</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+
         <Footer />
       </>
     );
