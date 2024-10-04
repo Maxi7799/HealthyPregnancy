1f41fd13d76d5a6eea1c96ffce4b4bb4571bda0d
Hao Wang <wh.tenghe@gmail.com>
2024-10-04 21:24:05 +1000

    fix: deploy

--- vite-project/env.tsx
+++ vite-project/env.tsx
@@ -1,7 +1,7 @@
 //While doing local dev: comment the first root Address and uncomment the second one
 
 //backend server connection
-// export const rootAddress = "https://healthypregancybackend.systems:8443";
+export const rootAddress = "https://healthypregancybackend.systems:8443";
 
 //localhost
-export const rootAddress = "http://localhost:8000"
+// export const rootAddress = "http://localhost:8000"
